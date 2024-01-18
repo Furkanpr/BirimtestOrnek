e9935207bd9ed2e42c3ec93bb31247525b3de062
Furki374 <fuurkandemiir@gmail.com>
2024-01-19 00:59:29 +0300

    .

--- Birimtestornek/src/main/java/tr/edu/medipol/hesaplamalar.java
+++ Birimtestornek/src/main/java/tr/edu/medipol/hesaplamalar.java
@@ -2,16 +2,10 @@ package tr.edu.medipol;
 
 public class hesaplamalar {
 
-	//public static void main(String[] args) {
+
 		
-		public static int faktoryelloop(int n) {
-				
-			int sonuc = 1; 
-			for(int i=n; i>1; i--) {
-				sonuc=sonuc*i;
-			}
-			return sonuc;
-		}
+		
+		
 		
 		public static int topla (int sayi1, int sayi2 , int sayi3) {
 			
